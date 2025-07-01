e944393a8aa7b5838b2cab47f2c3a1735d94d0ad
ritukanchi <ritukanchi04@gmail.com>
2025-07-01 12:39:18 +0530

    kafka server running - multinode using KRaft

---
+++ kafka_2.13-3.6.1/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+CONFIG="/opt/kafka/config/kraft/server${KAFKA_NODE_ID}.properties"
+LOG_DIR=$(grep '^log.dirs=' "$CONFIG" | cut -d'=' -f2 | tr -d '[:space:]')
+META_FILE="$LOG_DIR/meta.properties"
+
+if [ ! -f "$META_FILE" ]; then
+    echo "Formatting storage for node $KAFKA_NODE_ID..."
+    CLUSTER_ID="QBnJSwRaSK2FXE91KbRAew=="
+    echo "Using Cluster ID: $CLUSTER_ID"
+    /opt/kafka/bin/kafka-storage.sh format -t "$CLUSTER_ID" -c "$CONFIG"
+fi
+
+echo "Starting Kafka node $KAFKA_NODE_ID..."
+exec /opt/kafka/bin/kafka-server-start.sh "$CONFIG"
